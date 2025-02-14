02861f72f50b915603058a0dcc9edaae449bd1d8
Kashish Makkar <kashishmakkar99@gmail.com>
2025-02-15 00:00:30 +0530

    controller

--- src/main/java/com/bigmakk/quizapp/controller/QuestionController.java
+++ src/main/java/com/bigmakk/quizapp/controller/QuestionController.java
@@ -34,6 +34,7 @@ public class QuestionController {
 
     @PostMapping("add")
     public ResponseEntity<String>  addQuestion(@RequestBody Question question){
+
         return questionService.addQuestion(question);
     }
 }
